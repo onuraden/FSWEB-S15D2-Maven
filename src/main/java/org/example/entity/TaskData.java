3e740a7fd6a1b50ab3f167a0c1e46c71362149bb
onuraden <gurcayonur@outlook.com>
2025-05-22 08:51:56 +0300

    to be continued.

---
+++ src/main/java/org/example/entity/TaskData.java
@@ -0,0 +1,71 @@
+package org.example.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TaskData {
+    private Set<Task> annsTasks;
+    private Set<Task> bobsTasks;
+    private Set<Task> carolsTasks;
+    private Set<Task> unassignedTasks;
+
+    public TaskData(Set<Task> annsTasks, Set<Task> bobsTasks, Set<Task> carolsTasks, Set<Task> unassignedTasks) {
+        this.annsTasks = annsTasks;
+        this.bobsTasks = bobsTasks;
+        this.carolsTasks = carolsTasks;
+        this.unassignedTasks = unassignedTasks;
+    }
+
+    public Set<Task> getAnnsTasks() {
+        return annsTasks;
+    }
+
+    public Set<Task> getBobsTasks() {
+        return bobsTasks;
+    }
+
+    public Set<Task> getCarolsTasks() {
+        return carolsTasks;
+    }
+
+    public Set<Task> getUnassignedTasks() {
+        return unassignedTasks;
+    }
+
+    public Set<Task> getTasks(String employee) {
+        switch (employee.toLowerCase()) {
+            case "ann":
+                return annsTasks;
+            case "bob":
+                return bobsTasks;
+            case "carol":
+                return carolsTasks;
+            case "all":
+                Set<Task> unionAB = getUnion(annsTasks, bobsTasks);
+                return getUnion(unionAB, carolsTasks);
+            default:
+                return new HashSet<>();
+        }
+    }
+
+    public Set<Task> getUnion(Set<Task> set1, Set<Task> set2) {
+        Set<Task> union = new HashSet<>(set1);
+        union.addAll(set2);
+        return union;
+    }
+
+    public Set<Task> getIntersection(Set<Task> set1, Set<Task> set2) {
+        Set<Task> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+        return intersection;
+    }
+
+    public Set<Task> getDifferences(Set<Task> set1, Set<Task> set2) {
+        Set<Task> difference = new HashSet<>(set1);
+        difference.removeAll(set2);
+        return difference;
+    }
+
+
+
+   }
